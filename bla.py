561b403bdd815d4d3698f6188a4e90519fbf8a79
Martijn Stegeman <terra@proglab.nl>
2025-05-15 15:44:11 +0200

    Update bla.py

--- bla.py
+++ bla.py
@@ -1,4 +1,2 @@
-from checkpy.interactive import testOffline
-testOffline("sp1_exam1", "/home/pyodide", multiprocessing=False, silentMode=False)
-print("\nend tests!")
-
+import colorama
+colorama.init()
