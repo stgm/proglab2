bbb78db8bf4ff1ffc2e2c62dc593681b4559f7b0
Martijn Stegeman <terra@proglab.nl>
2025-05-15 15:45:54 +0200

    Update bla.py

--- bla.py
+++ bla.py
@@ -17,3 +17,14 @@ class _Smileys:
 def display(testResult: str) -> str:
     color, smiley = _selectColorAndSmiley(testResult)
     msg = "{}{} {}{}".format(color, smiley, testResult, _Colors.ENDC)
+
+def _selectColorAndSmiley(testResult: checkpy.tests.TestResult) -> typing.Tuple[str, str]:
+    if testResult.hasPassed:
+        return _Colors.PASS, _Smileys.HAPPY
+    if type(testResult.message) is exception.SourceException:
+        return _Colors.WARNING, _Smileys.CONFUSED
+    if testResult.hasPassed is None:
+        return _Colors.WARNING, _Smileys.NEUTRAL
+    return _Colors.FAIL, _Smileys.SAD
+
+display("fuck all")
